b51debcd5aafab0b2942e189bf25da354a9df83c
HookahLover <andriybenesh@gmail.com>
2020-03-29 11:34:35 +0300

    added Vasyl

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Test", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Vasyl", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
