843a9b4b18d5527a88be7d27ce0a48c4e84a459e
HookahLover <andriybenesh@gmail.com>
2020-03-30 17:06:16 +0300

    changed to Vasyl

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Vasylll", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Vasyl", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
